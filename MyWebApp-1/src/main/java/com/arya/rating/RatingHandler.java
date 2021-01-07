0a35eaddb2861234d25b7839aa86574acc48b0ea
aryaphan <aryaphan31@gmail.com>
2021-01-06 23:47:58 -0800

    added specs

--- MyWebApp-1/src/main/java/com/arya/rating/RatingHandler.java
+++ MyWebApp-1/src/main/java/com/arya/rating/RatingHandler.java
@@ -11,46 +11,21 @@ import com.arya.model.Sneaker;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * Handles and updates ratings of two pair of sneakers after one round of voting
+ * 
+ * */
 public class RatingHandler {
-	public static int getOldRating(int id) {
-		Connection con = ConnectionProvider.getConnection();
-		int votes = 0;
-		
-		try {
-			
-			PreparedStatement statement = con.prepareStatement("select score from sneakers where image_id=?");
-			System.out.println("getting old rating");
-			statement.setInt(1, id);
-			ResultSet result = statement.executeQuery();
-			
-			if(result.next()) {
-				votes = result.getInt(1);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return votes;
-		
-	}
 	
-	public static void updateRating(int id, int newRating) {
-		Connection con = ConnectionProvider.getConnection();
-		
-		try {
-			
-			PreparedStatement statement = con.prepareStatement("update sneakers set score=? where image_id=?");
-			System.out.println("updating rating");
-			statement.setInt(1, newRating);
-			statement.setInt(2, id);
-			statement.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
+	/*
+	 * Gets new ratings of two pair of sneakers and updates their ratings in the database
+	 * 
+	 * @param id1 the identifier of sneakers A, is not null. id1 >= 1 and id1 != id2
+	 * @param id2 the identifier of sneakers B, is not null. id2 >= 1 and id2 != id1
+	 * @param result true if sneakers A win, otherwise false
+	 * 
+	 * */
 	public static void getNewRating(int id1, int id2, boolean result) {
-		//result = true if sneaker 1 wins, otherwise, false
 		int prevRatingA = getOldRating(id1);
 		int prevRatingB = getOldRating(id2);
 		
@@ -63,7 +38,12 @@ public class RatingHandler {
 		System.out.println("getting new rating");
 	}
 	
-	public static ArrayList<Sneaker> getRanking() {
+	/*
+	 * Returns a list of all sneakers that are sorted based on their scores in descending order in the database
+	 * 
+	 * @return: a list of all sneakers that are sorted based on scores in descending order
+	 * */
+	public static List<Sneaker> getRanking() {
 		ArrayList<Sneaker> sneakerList = new ArrayList<Sneaker>();
 		
 		Connection con = ConnectionProvider.getConnection();
@@ -86,6 +66,59 @@ public class RatingHandler {
 		return sneakerList;
 		
 	}
+	
+	/*
+	 * Gets the current rating (before voting) in MySQL database of a pair of sneakers
+	 * 
+	 * @param: id the identifier of the sneakers, is not null. id >= 1
+	 * @return: the current rating (before voting) of the sneakers
+	 * 
+	 * */
+	private static int getOldRating(int id) {
+		Connection con = ConnectionProvider.getConnection();
+		int score = 0;
+		
+		try {
+			
+			PreparedStatement statement = con.prepareStatement("select score from sneakers where image_id=?");
+			System.out.println("getting old rating");
+			statement.setInt(1, id);
+			ResultSet result = statement.executeQuery();
+			
+			if(result.next()) {
+				score = result.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return score;
+		
+	}
+	
+	/*
+	 * Updates the rating of a pair of sneakers in the database
+	 * 
+	 * @param: id the identifier of the sneakers, is not null. id >= 1
+	 * @param newRating the new rating of the sneakers, is not null. newRating >= 0
+	 * 
+	 * */
+	private static void updateRating(int id, int newRating) {
+		Connection con = ConnectionProvider.getConnection();
+		
+		try {
+			
+			PreparedStatement statement = con.prepareStatement("update sneakers set score=? where image_id=?");
+			System.out.println("updating rating");
+			statement.setInt(1, newRating);
+			statement.setInt(2, id);
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
 
 
