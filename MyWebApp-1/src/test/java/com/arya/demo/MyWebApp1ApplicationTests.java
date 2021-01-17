adf7d7ec669febe30203440d2cd59155ed81a9cf
aryaphan <aryaphan31@gmail.com>
2021-01-16 23:37:46 -0800

    minor changes

--- MyWebApp-1/src/test/java/com/arya/demo/MyWebApp1ApplicationTests.java
+++ MyWebApp-1/src/test/java/com/arya/demo/MyWebApp1ApplicationTests.java
@@ -3,7 +3,9 @@ package com.arya.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.arya.application.MyWebApp1Application;
+
+@SpringBootTest(classes = MyWebApp1Application.class)
 class MyWebApp1ApplicationTests {
 
 	@Test
