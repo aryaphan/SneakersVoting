4994739b0fa0c3dcf0040ad61b9f4b3e6cef79c2
aryaphan <aryaphan31@gmail.com>
2021-01-21 00:08:26 -0800

    added exceptions

---
+++ MyWebApp-1/src/main/java/com/arya/exception/FileNotFoundException.java
@@ -0,0 +1,13 @@
+package com.arya.exception;
+
+public class FileNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public FileNotFoundException(String message) {
+		super(message);
+	}
+	
+	public FileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
