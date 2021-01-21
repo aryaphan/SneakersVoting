6d1460d979855d1afd998b68a699c460a43c5afd
aryaphan <aryaphan31@gmail.com>
2021-01-21 00:09:30 -0800

    debugged where some packages were not scanned properly

--- MyWebApp-1/src/main/java/com/arya/application/MyWebApp1Application.java
+++ MyWebApp-1/src/main/java/com/arya/application/MyWebApp1Application.java
@@ -2,6 +2,7 @@ package com.arya.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,8 +10,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import com.arya.dao.SneakerDAO;
 
 
-@SpringBootApplication(scanBasePackages = {"com.arya.application", "com.arya.dao", "com.arya.servlet"})
+@SpringBootApplication(scanBasePackages = {"com.arya.application", "com.arya.dao", "com.arya.servlet", "com.arya.service", "com.arya.repository"})
 @ServletComponentScan("com.arya.servlet")
+//@ComponentScan(basePackages = {"com.arya.service", "com.arya.dao", "com.arya.repository"})
+@EnableJpaRepositories("com.arya.repository")
+@EntityScan("com.arya.model")
 public class MyWebApp1Application {
 
 	public static void main(String[] args) {
